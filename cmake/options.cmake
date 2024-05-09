5dc9a1e389907e0895a3f93318abbd6c9f33f95e
scivision <scivision@users.noreply.github.com>
2024-05-09 19:18:59 -0400

    cmake < 3.21 top_level

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,7 +2,14 @@ include(GNUInstallDirs)
 
 message(STATUS "${PROJECT_NAME} ${PROJECT_VERSION}  CMake ${CMAKE_VERSION} Toolchain ${CMAKE_TOOLCHAIN_FILE}")
 
-option(${PROJECT_NAME}_BUILD_TESTING "Build tests" ${PROJECT_IS_TOP_LEVEL})
+if(CMAKE_VERSION VERSION_LESS 3.21)
+  get_property(not_top DIRECTORY PROPERTY PARENT_DIRECTORY)
+  if(NOT not_top)
+    set(${PROJECT_NAME}_IS_TOP_LEVEL true)
+  endif()
+endif()
+
+option(${PROJECT_NAME}_BUILD_TESTING "Build tests" ${${PROJECT_NAME}_IS_TOP_LEVEL})
 
 # Necessary for shared library with Visual Studio / Windows oneAPI
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
