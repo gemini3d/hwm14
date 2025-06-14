22b262d617512eebf549ba7caa9bcae1bef7830c
scivision <scivision@users.noreply.github.com>
2025-06-13 20:21:50 -0400

    avoid -Wall compiler bugs in future

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,12 +1,9 @@
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
-  add_compile_options(-Wall -fimplicit-none)
+  add_compile_options("$<$<CONFIG:Debug,RelWithDebInfo>:-Wall>"
+  -fimplicit-none
+  )
   # $<$<CONFIG:Debug>:-fcheck=all>  fails in line 1246 array bounds error
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
   add_compile_options(-traceback -warn)
     #"$<$<CONFIG:Debug>:-check>" same as gfortran
-  if(WIN32)
-    add_compile_options($<$<CONFIG:Debug>:/Od>)
-  else()
-    add_compile_options($<$<CONFIG:Debug>:-O0>)
-  endif()
 endif()
